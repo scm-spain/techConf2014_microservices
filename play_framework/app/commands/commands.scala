a20ec79b07f0e4d93729a38dd7590d35f872e750
Jaume Pinyol <jaume.pinyol@scmspain.com>
2014-11-28 12:47:36 +0100

    play framework

--- play_framework/app/commands/commands.scala
+++ play_framework/app/commands/commands.scala
@@ -5,6 +5,7 @@ import com.netflix.hystrix.HystrixCommand.Setter
 import com.netflix.hystrix.{HystrixCommand, HystrixCommandGroupKey, HystrixCommandKey}
 import com.netflix.loadbalancer.Server
 import controllers.RibbonCommand
+import models.User
 import play.api.libs.json.{JsValue, Json}
 
 object MicroServiceACommand {
@@ -15,20 +16,26 @@ object MicroServiceACommand {
   def apply() = new MicroServiceACommand
 }
 
-class MicroServiceACommand extends HystrixCommand[Int](MicroServiceACommand.key) with BogusHelper {
+class MicroServiceACommand extends HystrixCommand[User](MicroServiceACommand.key) with BogusHelper {
 
-  def run(): Int = {
-    //simulateNetworkLatency(50, 125)
+  def run(): User = {
+    simulateNetworkLatency(500, 2000)
     simulateFailUnderLoad(0.01)
-    //simulateNetworkSpike(15, 650, 2250)
-    val ribbon = new RibbonCommand(Lists.newArrayList(new Server("localhost", 8888)))
-    val responseContent = ribbon.call("/friends/")
+    simulateNetworkSpike(15, 650, 2250)
+    val ribbon = new RibbonCommand(Lists.newArrayList(new Server("localhost", 8887)))
+    val responseContent = ribbon.call("/user/4")
     val json: JsValue = Json.parse(responseContent)
-    val friends2 = (json \ "friends").as[List[String]]
-    between(20, 35)
+
+    val userJson = (json \ "user")
+    val gender = (json \ "user" \ "gender").as[String];
+    val name = (json \ "user" \ "name").as[String];
+    val surname = (json \ "user" \ "surname").as[String];
+    val email = (json \ "user" \ "email").as[String];
+
+    new User(gender, name, surname, email)
   }
 
-  override def getFallback: Int = 0
+  override def getFallback: User = new User("", "", "", "")
 }
 
 object MicroServiceBCommand {
@@ -42,9 +49,9 @@ object MicroServiceBCommand {
 class MicroServiceBCommand extends HystrixCommand[List[String]](MicroServiceBCommand.key) with BogusHelper {
 
   def run(): List[String] = {
-    //simulateNetworkLatency(50, 125)
+    simulateNetworkLatency(500, 1250)
     simulateFailUnderLoad(0.01)
-    //simulateNetworkSpike(15, 650, 2250)
+    simulateNetworkSpike(15, 650, 2250)
     val ribbon = new RibbonCommand(Lists.newArrayList(new Server("localhost", 8888)))
     val responseContent = ribbon.call("/friends/Chuck-norris")
     val json: JsValue = Json.parse(responseContent)
@@ -52,5 +59,5 @@ class MicroServiceBCommand extends HystrixCommand[List[String]](MicroServiceBCom
     friends2
   }
 
-  override def getFallback: List[String] = List("No friends")
+  override def getFallback: List[String] = List();
 }
