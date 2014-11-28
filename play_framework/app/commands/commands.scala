52569937129ddf0ceb81c99ffde48ab399104790
Jaume Pinyol <jaume.pinyol@scmspain.com>
2014-11-28 11:03:40 +0100

    play framework

--- play_framework/app/commands/commands.scala
+++ play_framework/app/commands/commands.scala
@@ -1,7 +1,11 @@
 package commands
 
+import com.google.common.collect.Lists
 import com.netflix.hystrix.HystrixCommand.Setter
 import com.netflix.hystrix.{HystrixCommand, HystrixCommandGroupKey, HystrixCommandKey}
+import com.netflix.loadbalancer.Server
+import controllers.RibbonCommand
+import play.api.libs.json.{JsValue, Json}
 
 object MicroServiceACommand {
   private final def key = Setter
@@ -14,10 +18,13 @@ object MicroServiceACommand {
 class MicroServiceACommand extends HystrixCommand[Int](MicroServiceACommand.key) with BogusHelper {
 
   def run(): Int = {
-    simulateNetworkLatency(50, 125)
+    //simulateNetworkLatency(50, 125)
     simulateFailUnderLoad(0.01)
-    simulateNetworkSpike(15, 650, 2250)
-
+    //simulateNetworkSpike(15, 650, 2250)
+    val ribbon = new RibbonCommand(Lists.newArrayList(new Server("localhost", 8888)))
+    val responseContent = ribbon.call("/friends/")
+    val json: JsValue = Json.parse(responseContent)
+    val friends2 = (json \ "friends").as[List[String]]
     between(20, 35)
   }
 
@@ -32,15 +39,18 @@ object MicroServiceBCommand {
   def apply() = new MicroServiceBCommand
 }
 
-class MicroServiceBCommand extends HystrixCommand[Int](MicroServiceBCommand.key) with BogusHelper {
+class MicroServiceBCommand extends HystrixCommand[List[String]](MicroServiceBCommand.key) with BogusHelper {
 
-  def run(): Int = {
-    simulateNetworkLatency(50, 125)
+  def run(): List[String] = {
+    //simulateNetworkLatency(50, 125)
     simulateFailUnderLoad(0.01)
-    simulateNetworkSpike(15, 650, 2250)
-    
-    between(20, 35)
+    //simulateNetworkSpike(15, 650, 2250)
+    val ribbon = new RibbonCommand(Lists.newArrayList(new Server("localhost", 8888)))
+    val responseContent = ribbon.call("/friends/Chuck-norris")
+    val json: JsValue = Json.parse(responseContent)
+    val friends2 = (json \ "friends").as[List[String]]
+    friends2
   }
 
-  override def getFallback: Int = 0
+  override def getFallback: List[String] = List("No friends")
 }
---
+++ play_framework/app/controllers/RibbonCommand.java
@@ -0,0 +1,73 @@
+package controllers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import rx.Observable;
+
+import com.google.common.collect.Lists;
+import com.netflix.client.DefaultLoadBalancerRetryHandler;
+import com.netflix.client.RetryHandler;
+import com.netflix.loadbalancer.BaseLoadBalancer;
+import com.netflix.loadbalancer.ILoadBalancer;
+import com.netflix.loadbalancer.LoadBalancerBuilder;
+import com.netflix.loadbalancer.LoadBalancerStats;
+import com.netflix.loadbalancer.Server;
+import com.netflix.loadbalancer.reactive.LoadBalancerCommand;
+import com.netflix.loadbalancer.reactive.ServerOperation;
+
+public class RibbonCommand {
+
+    private final ILoadBalancer loadBalancer;
+    // retry handler that does not retry on same server, but on a different server
+    private final RetryHandler retryHandler = new DefaultLoadBalancerRetryHandler(0, 1, true);
+
+    public RibbonCommand(List<Server> serverList) {
+        loadBalancer = LoadBalancerBuilder.newBuilder().buildFixedServerListLoadBalancer(serverList);
+    }
+
+    public String call(final String path) throws Exception {
+        return LoadBalancerCommand.<String>builder()
+                .withLoadBalancer(loadBalancer)
+                .build()
+                .submit(new ServerOperation<String>() {
+                    @Override
+                    public Observable<String> call(Server server) {
+                        URL url;
+                        try {
+                            url = new URL("http://" + server.getHost() + ":" + server.getPort() + path);
+                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                            return Observable.just(getContent(conn));
+                        } catch (Exception e) {
+                            return Observable.error(e);
+                        }
+                    }
+                }).toBlocking().first();
+    }
+
+    public LoadBalancerStats getLoadBalancerStats() {
+        return ((BaseLoadBalancer) loadBalancer).getLoadBalancerStats();
+    }
+
+    private String getContent(HttpURLConnection connection){
+        try {
+            BufferedReader bufferReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = bufferReader.readLine()) != null) {
+                stringBuilder.append(line + "\n");
+            }
+            bufferReader.close();
+            return stringBuilder.toString();
+        } catch (IOException exception){
+            return "";
+        }
+    }
+
+
+}
