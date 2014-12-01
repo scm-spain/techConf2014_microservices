6af3a9d1b6e4faf6361ac2899f1c2c6a74ac34e9
Jaume Pinyol <jaume.pinyol@scmspain.com>
2014-12-01 19:46:15 +0100

    play framework to complete

--- play_framework/app/controllers/Aggregator.scala
+++ play_framework/app/controllers/Aggregator.scala
@@ -6,7 +6,8 @@ import play.api.mvc.{AnyContent, Request}
 object Aggregator extends CommandStreamController {
 
   def pagelets(request: Request[AnyContent]) =
-      MicroServiceUser.pagelets(request) ::: MicroServiceFriends.pagelets(request) ::: MicroServiceAdd.pagelets(request)
-
+      //MicroServiceUser.pagelets(request) ::: MicroServiceFriends.pagelets(request) ::: MicroServiceAdd.pagelets(request)
+      //MicroServiceUser.pagelets(request) ::: MicroServiceAdd.pagelets(request)
+      MicroServiceAdd.pagelets(request)
   val layout = views.pagelet.aggregated.main
 }
--- play_framework/app/controllers/MicroServiceUser.scala
+++ play_framework/app/controllers/MicroServiceUser.scala
@@ -1,13 +1,12 @@
 package controllers
 
-import commands.MicroServiceUserCommand
 import play.api.mvc.{AnyContent, Request}
 import views.html.microserviceuser.microServiceUser
 
 object MicroServiceUser extends CommandStreamController {
 
   def pagelets(request: Request[AnyContent]) = {
-    val microserviceUser = command(MicroServiceUserCommand(), microServiceUser.apply, "microserviceUser")
+    val microserviceUser = command(???, microServiceUser.apply, "microserviceUser")
     List(microserviceUser)
   }
 
