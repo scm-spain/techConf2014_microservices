93f537e8c04de85ee2b692ab6aefb54d9288450f
ramonibz <ramonibz@gmail.com>
2014-12-01 16:48:20 +0100

    Convert AddEnpoint to reactive

--- microserviceA_Solution/src/main/java/io/scmspain/tech2014/server/simple/AddEndpoint.java
+++ microserviceA_Solution/src/main/java/io/scmspain/tech2014/server/simple/AddEndpoint.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rx.Observable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -17,22 +18,42 @@ public class AddEndpoint {
     private static final Logger logger = LoggerFactory.getLogger(AddEndpoint.class);
 
 
+    /**
+     * Reactive endpoint that su8ms two given numbers and writes
+     * the result to the response in JSON format
+     * @param request
+     * @param response
+     * @return
+     */
     public Observable<Void> add(HttpServerRequest<ByteBuf> request, HttpServerResponse<ByteBuf> response) {
-        JSONObject content = new JSONObject();
-        try {
-            Map<String, List<String>> params = request.getQueryParameters();
-            int addend1 = Integer.parseInt(params.get("addend1").get(0));
-            int addend2 = Integer.parseInt(params.get("addend2").get(0));
-            content.put("result", addend1 + addend2);
+        Observable o = getAddEnds(request).reduce((a,b) -> a + b).flatMap(a -> {
+            JSONObject content = new JSONObject();
+            try{
+                content.put("result", a);
+            } catch (Exception e) {
+                logger.error("Error creating json response.", e);
+                return Observable.error(e);
+            }
+            response.write(content.toString(), StringTransformer.DEFAULT_INSTANCE);
+            return response.close();
+        });
+        return o;
+    }
 
-        } catch (Exception e) {
-            logger.error("Error creating json response.", e);
-            return Observable.error(e);
-        }
+    /**
+     * Returns a List with the values to sum
+     * @param request
+     * @return
+     */
+    private Observable<Integer> getAddEnds(HttpServerRequest<ByteBuf> request){
+        Map<String, List<String>> params = request.getQueryParameters();
+        Integer addend1 = Integer.parseInt(params.get("add1").get(0));
+        Integer addend2 = Integer.parseInt(params.get("add2").get(0));
 
-        response.write(content.toString(), StringTransformer.DEFAULT_INSTANCE);
-        return response.close();
-
+        List<Integer> addEnds = new ArrayList<>();
+        addEnds.add(addend1);
+        addEnds.add(addend2);
+        return Observable.from(addEnds);
     }
 
 
